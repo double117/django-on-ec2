0c7277871463101d002a0b5c07a6655c2c9cf188
double117 <matlesslie@gmail.com>
2025-01-05 20:47:16 -0600

    homepage bs

--- homepage/urls.py
+++ homepage/urls.py
@@ -22,6 +22,7 @@ from . import settings, views
 urlpatterns = [
     path('todos/', include('todos.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index)
+    path('', views.index),
+    path('homepage/', include('homepage.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 urlpatterns += staticfiles_urlpatterns()
--- todoApp/settings.py
+++ todoApp/settings.py
@@ -32,7 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'todos.apps.TodosConfig',
-    #'homepage.apps.HomepageConfig',
+    'homepage.apps.HomepageConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
